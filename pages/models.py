fa314ec760a0886a53704a679acd2e63a00ca21b
Kairo Amorim <kairo.amorim2001@gmail.com>
2022-06-27 20:01:02 -0300

    Classes/Modelos
    Inicio da implementação das classes/models do projeto, criadas a partir do diagrama de classes

--- pages/models.py
+++ pages/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -28,3 +29,84 @@ class Pessoa(models.Model):
 
     def __str__(self):
         return self.nome_completo
+
+
+class Sprite(models.Model):
+    name = models.CharField(max_length=255)
+    base64 = models.CharField(max_length=5000)
+    type = models.CharField(max_length=50)
+
+    active = models.BooleanField(default=True)
+    registered_in = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+
+class Element(models.Model):
+    name = models.CharField(max_length=255)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Effect(models.Model):
+    name = models.CharField(max_length=255)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Equipment(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.type} {self.name}'
+
+
+class Warrior(models.Model):
+    name = models.CharField(max_length=255)
+    # sprite = models.ManyToManyField(choices=Sprite)
+
+    active = models.BooleanField(default=True)
+    registered_in = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+
+class Spell(models.Model):
+    name = models.CharField(max_length=255)
+    mana_cost = models.FloatField()
+
+    active = models.BooleanField(default=True)
+    registered_in = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+
+class Attributes(models.Model):
+    health_points = models.FloatField()
+    mana = models.FloatField()
+    strength = models.FloatField()
+    xp = models.FloatField()
+    armor = models.FloatField()
+    crit_rate = models.FloatField()
+
+    active = models.BooleanField(default=True)
+    registered_in = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return '{:.2f} - Atributos: {:.2f} | {:.2f} | {:.2f} | {:.2f} | {:.2f} | {:.2f}' \
+            .format(self.pk, self.health_points, self.mana, self.strength, self.xp, self.armor, self.crit_rate)
