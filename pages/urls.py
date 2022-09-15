e372bd037d655f09a57d5f22c14e03c4de3c529a
Kairo Amorim <kairo.amorim2001@gmail.com>
2022-09-14 22:44:58 -0300

    Estilização cadastro de usuário

--- pages/urls.py
+++ pages/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import Index
-from .views import CidadeCreate, PessoaCreate
+from .views import CidadeCreate, PessoaCreate, UserCreate
 from .views import CidadeUpdate, PessoaUpdate
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('register/cidade', CidadeCreate.as_view(), name='register-cidade'),
     path('register/pessoa', PessoaCreate.as_view(), name='register-pessoa'),
+    path('register/user', UserCreate.as_view(), name='register-user'),
     path('edit/cidade/<int:pk>', CidadeUpdate.as_view(), name='edit-cidade'),
     path('edit/pessoa/<int:pk>', PessoaUpdate.as_view(), name='edit-pessoa'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView, UpdateView
 from .models import Cidade, Pessoa
 from braces.views import GroupRequiredMixin
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -23,7 +24,7 @@ class CidadeCreate(GroupRequiredMixin, CreateView):
 class PessoaCreate(CreateView, UpdateView):
     model = Pessoa
     fields = ['nome_completo', 'idade', 'email', 'cidade']
-    template_name = 'register/form.html'
+    template_name = 'register/register_account.html'
     success_url = reverse_lazy('index')
 
 
@@ -38,4 +39,11 @@ class PessoaUpdate(UpdateView):
     model = Pessoa
     fields = ['nome_completo', 'idade', 'email', 'cidade']
     template_name = 'register/form.html'
+    success_url = reverse_lazy('index')
+
+
+class UserCreate(CreateView):
+    model = User
+    fields = ["first_name", "last_name", "email", "password"]
+    template_name = 'register/register_account.html'
     success_url = reverse_lazy('index')
